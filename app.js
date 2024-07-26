d66ac1ce13e31b6db8cd19eee14a8104aa20c325
Marcelo Harger Flores <mharger.86@gmail.com>
2024-07-26 10:53:57 -0300

    Passo 07 - criada função SORTEAR

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 function sortear(){
-    let quantidade = document.getElementById("quantidade").value;
-    let de = document.getElementById("de").value;
-    let ate = document.getElementById("ate").value;
-    console.log(quantidade, de, ate);
-    alert(`Quantidade: ${quantidade}, de: ${de}, até: ${ate}.`);
-};
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = parseInt(document.getElementById("de").value);
+    let ate = parseInt(document.getElementById("ate").value);
+
+    console.log(`Sortear ${quantidade} número(s) entre ${de} e ${ate}.`);
+
+   // console.log(`Número sorteado: ${numero}.`);
+
+   let sorteados = [];
+   let numero;
+
+   for (let i = 0; i < quantidade; i++){
+    numero = obterNumeroAleatorio(de, ate);
+    sorteados.push(numero);
+   }
+
+   console.log(`Número sorteado: ${sorteados}.`);
+
+}
+
+function obterNumeroAleatorio(min, max) { //a função abaixo foi introduzida de https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+      
+
+}
